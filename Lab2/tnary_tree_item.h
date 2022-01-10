8728460dac418c108fff15adeb9b3c25ecdbe8c7
Silvestr <90452368+silverfatt@users.noreply.github.com>
2022-01-11 00:59:47 +0300

    Update tnary_tree_item.h

--- Lab2/tnary_tree_item.h
+++ Lab2/tnary_tree_item.h
@@ -2,15 +2,16 @@
 #define TNARY_TREE_ITEM_H
 #include "rhombus.h"
 
-class TnaryTreeItem {
- public:
- public:
+class TnaryTreeItem
+{
+public:
+public:
   TnaryTreeItem();
-  TnaryTreeItem(Rhombus& rhombus);
-  TnaryTreeItem* son;
-  TnaryTreeItem* brother;
+  TnaryTreeItem(Rhombus &rhombus);
+  TnaryTreeItem *son;
+  TnaryTreeItem *brother;
   virtual ~TnaryTreeItem();
   Rhombus rhombus;
 };
 
-#endif  // TNARY_TREE_ITEM_H
+#endif // TNARY_TREE_ITEM_H
